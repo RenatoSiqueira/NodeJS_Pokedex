81f9016bfe079afb23cb1dead9ee8784b466c1cf
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-09-23 10:24:34 -0300

    Swagger Docs Added

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 require('dotenv').config()
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
 const app = express()
 
-const { PORT } = process.env
+const PORT = process.env.PORT || 3000
 const Routes = require('./routes')
+
+const swaggerDoc = YAML.load('./swagger.yaml')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.use(Routes)
 
